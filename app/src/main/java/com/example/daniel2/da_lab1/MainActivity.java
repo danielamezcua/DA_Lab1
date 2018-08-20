2f1f0b9f3905ab6834ddb847c9cb1d5aed885456
daniel amezcua <dany_amezcua@live.com.mx>
2018-08-20 17:34:58 -0500

    Lab1

--- app/src/main/java/com/example/daniel2/da_lab1/MainActivity.java
+++ app/src/main/java/com/example/daniel2/da_lab1/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.daniel2.da_lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.util.Log;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doggo(View v) {
+        Intent intent = new Intent(MainActivity.this, Activity2.class);
+        startActivity(intent);
     }
 }
