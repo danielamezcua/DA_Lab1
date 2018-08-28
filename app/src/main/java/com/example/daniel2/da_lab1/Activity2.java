4923325bfe89978960f7f54cbb1e250c139e31ea
daniel amezcua <dany_amezcua@live.com.mx>
2018-08-27 19:22:00 -0500

    Create Lab2

--- app/src/main/java/com/example/daniel2/da_lab1/Activity2.java
+++ app/src/main/java/com/example/daniel2/da_lab1/Activity2.java
@@ -1,7 +1,9 @@
 package com.example.daniel2.da_lab1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        /*Obtain text from intent, if any */
+        Intent intent = getIntent();
+
+        String text = intent.getStringExtra("text");
+
+        /*Set text if any */
+        TextView textView = findViewById(R.id.varText);
+        textView.setText("You said: " + text + " to the doggo.");
     }
 }
--- app/src/main/java/com/example/daniel2/da_lab1/MainActivity.java
+++ app/src/main/java/com/example/daniel2/da_lab1/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.daniel2.da_lab1;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.util.Log;
 import android.content.Intent;
-
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -17,8 +16,29 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
     }
 
-    public void doggo(View v) {
+    public void talk_to_doggo(View v){
         Intent intent = new Intent(MainActivity.this, Activity2.class);
+        /*Obtain text*/
+        EditText editText = findViewById(R.id.editText);
+        String text = editText.getText().toString();
+
+        /*Set text to activity 2*/
+        intent.putExtra("text", text);
+
+        /*Start activity 2*/
+        startActivity(intent);
+    }
+
+    public void change_text(View v){
+       /* obtain text view */
+       TextView textView = findViewById(R.id.textView3);
+
+       /*Change text */
+       textView.setText("Casual conversations doesn't mean that she likes you.");
+    }
+
+    public void doggo(View v) {
+        Intent intent = new Intent(MainActivity.this, activity_3.class);
         startActivity(intent);
     }
 }
